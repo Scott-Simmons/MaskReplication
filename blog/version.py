3e25b2751f4a73dbcf5a8093ca8204dd68f73f22
Scott-Simmons <52365471+Scott-Simmons@users.noreply.github.com>
2026-04-13 22:40:28 +1000

    Reset to version 1

--- blog/version.py
+++ blog/version.py
@@ -1,13 +1,8 @@
 """Blog post versioning and changelog."""
 
-VERSION = "2"
+VERSION = "1"
 
 # (version, date, [items])
 CHANGELOG: list[tuple[str, str, list[str]]] = [
-    ("2", "2026-04-12", [
-        "Add P(honest) and P(lie) comparison tables to appendix",
-        "Fix changelog back-link",
-        "Byline links to sdsimmons.com",
-    ]),
-    ("1", "2026-04-05", ["Initial blog post publication"]),
+    ("1", "2026-04-13", ["Initial blog post publication"]),
 ]
